8c2c0df05aef1f7bb206d1525c433479f7c569db
Kronda <ephany@gmail.com>
2013-05-27 09:29:33 -0700

    made icons slightly smaller

--- wordpress/wp-content/themes/karveldigital/js/karvel.js
+++ wordpress/wp-content/themes/karveldigital/js/karvel.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
   jQuery('.home h1').addClass('visuallyhidden');
   jQuery('.gallery-icon a').attr('href', '/portfolio');
-  jQuery('.widget_woothemes_features .feature:nth-child(2) .feature-title').before('<i class="icon-wrench icon-3x"></i>');
-  jQuery('.widget_woothemes_features .last .feature-title').before('<i class="icon-group icon-3x"></i>');
+  jQuery('.widget_woothemes_features .feature:nth-child(2) .feature-title').before('<i class="icon-wrench icon-2x"></i>');
+  jQuery('.widget_woothemes_features .last .feature-title').before('<i class="icon-group icon-2x"></i>');
 });

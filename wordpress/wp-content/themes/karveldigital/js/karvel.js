8ba50c0d1b84d2bff6244f296f7c54b141d81d90
Kronda <ephany@gmail.com>
2013-05-27 11:23:02 -0700

    fixed home page logo

--- wordpress/wp-content/themes/karveldigital/js/karvel.js
+++ wordpress/wp-content/themes/karveldigital/js/karvel.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-  jQuery('.home h1').addClass('visuallyhidden');
+  jQuery('.home .page-content h1').addClass('visuallyhidden');
   jQuery('.gallery-icon a').attr('href', '/portfolio');
   jQuery('.widget_woothemes_features .feature:nth-child(2) .feature-title').before('<i class="icon-wrench icon-2x"></i>');
   jQuery('.widget_woothemes_features .last .feature-title').before('<i class="icon-group icon-2x"></i>');

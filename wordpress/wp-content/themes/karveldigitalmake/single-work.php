9f5fab86cd34505634f20dad438c16ef8f9577ab
Kronda <ephany@gmail.com>
2014-06-10 07:00:31 -0700

    work template page styles

--- wordpress/wp-content/themes/karveldigitalmake/single-work.php
+++ wordpress/wp-content/themes/karveldigitalmake/single-work.php
@@ -11,6 +11,10 @@ get_header();
 <?php ttfmake_maybe_show_sidebar( 'left' ); ?>
 
 <main id="site-main" class="site-main" role="main">
+<?php if ( function_exists('yoast_breadcrumb') ) {
+  yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+} ?>
+
 <?php if ( have_posts() ) : ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>

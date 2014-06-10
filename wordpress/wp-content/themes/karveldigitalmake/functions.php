3512a57b4e8b668b07bfad23b6650706b4c8a5ff
Kronda <ephany@gmail.com>
2014-06-10 05:28:29 -0700

    eleminate work category from blog

--- wordpress/wp-content/themes/karveldigitalmake/functions.php
+++ wordpress/wp-content/themes/karveldigitalmake/functions.php
@@ -2,3 +2,12 @@
 /**
  * @package Make Karvel Digital
  */
+
+/** Exclude Portfolio category from posts */
+add_action( 'pre_get_posts', 'karvel_exclude_category_from_blog' );
+function karvel_exclude_category_from_blog( $query ) {
+
+    if( $query->is_main_query() && $query->is_home() ) {
+        $query->set( 'cat', '-31' ); // cat id #s
+    }
+}

aa0dff3b07657cf10c841a67f42141b809e3ebda
kronda <kronda@karveldigital.com>
2016-01-31 12:49:27 -0800

    old karveldigital theme deleted

--- wordpress/wp-content/themes/karveldigital/js/karvel.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-  jQuery('.home .page-content h1').addClass('visuallyhidden');
-  jQuery('.gallery-icon a').attr('href', '/portfolio');
-  jQuery('.widget_woothemes_features .feature:nth-child(2) .feature-title').before('<i class="icon-wrench icon-2x"></i>');
-  jQuery('.widget_woothemes_features .last .feature-title').before('<i class="icon-group icon-2x"></i>');
-});
